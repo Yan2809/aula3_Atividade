4056b1f33f3db5938bee154f5faef61df5c841b0
Yan Breno <ybarutti2809@gmail.com>
2025-03-27 09:18:36 -0300

    Conclusão atividade 27/03

---
+++ script.js
@@ -0,0 +1,7 @@
+let nome = prompt("Digite seu nome: ")
+let anoNasci = prompt("Digite o ano que você nasceu: ")
+
+let anoAtual = 2025
+let idade = parseInt(anoAtual) - parseInt(anoNasci)
+
+alert(`Olá ${nome}, você tem ${idade} anos`)
